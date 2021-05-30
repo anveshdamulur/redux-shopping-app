09c5a2d459eda05612bdbb394fc9ce0e3b83752a
anveshdamulur <damulurianvesh2612@gmail.com>
2021-05-30 13:16:13 +0200

    Link to logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Header = () => {
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
-                <h2>fake Shop</h2>
+                <Link to="/">
+                    <h2>fake Shop</h2>
+                </Link>
             </div>
         </div>
     )
